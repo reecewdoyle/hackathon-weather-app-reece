2464093c8d37cbde6dfef393e61b65d47ce82652
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-05-27 23:16:06 +1000

    feat: added video timestamp and link

--- src/components/weather/index.jsx
+++ src/components/weather/index.jsx
@@ -4,8 +4,8 @@ import Search from "../search";
 
 export default function Weather(){
     const [search, setSearch] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [weatherData, setWeatherData] =useState(null);
+    // const [loading, setLoading] = useState(false);
+    // const [weatherData, setWeatherData] =useState(null);
 
     async function handleSearch() {}
 
@@ -18,3 +18,6 @@ export default function Weather(){
         Weather
     </div>
 }
+
+// 5:29:54 is where I got up to in the video 
+// https://www.youtube.com/watch?v=5ZdHfJVAY-s
