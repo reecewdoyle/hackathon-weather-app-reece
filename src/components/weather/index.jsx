6aa7cf5ed286eaf6cda3b3a54a06c14efc1d0a31
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-05-28 13:01:29 +1000

    feat: api used, Boolaroo as default

--- src/components/weather/index.jsx
+++ src/components/weather/index.jsx
@@ -1,13 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "../search";
-
 
 export default function Weather(){
     const [search, setSearch] = useState('');
-    // const [loading, setLoading] = useState(false);
-    // const [weatherData, setWeatherData] =useState(null);
+    const [loading, setLoading] = useState(false);
+    const [weatherData, setWeatherData] =useState(null);
 
-    async function handleSearch() {}
+    async function fetchWeatherData(param){
+        setLoading(true);
+        try {
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=3fb2a2b79b0555e36a6dd372db7ffb47`
+            );
+
+            const data = await response.json();
+            if(data) {
+                setWeatherData(data)
+                setLoading(false)
+            }
+        }catch(e){
+            setLoading(false)
+            console.log(e);
+        }
+    }
+
+    async function handleSearch() {
+        fetchWeatherData(search); 
+    }
+
+    function getCurrentDate() {
+        return new Date().toLocaleDateString("en-us", {
+          weekday: "long",
+          day: "numeric",
+          month: "long",
+          year: "numeric",
+        });
+      }
+
+    useEffect(() => {
+        fetchWeatherData('Boolaroo');
+      }, []);
+    
+      console.log(weatherData);
 
     return <div>
         <Search
@@ -15,9 +49,21 @@ export default function Weather(){
         setSearch={setSearch}
         handleSearch={handleSearch}
         />
-        Weather
+        {
+            loading ? <div>Loading...</div> :
+            <div>
+                <div className="city-name">
+                    <h2>{weatherData?.name}, <span>{weatherData?.sys?.country}</span></h2>
+                </div>
+                <div className="date">
+                    <span>{getCurrentDate()}</span>
+                </div>
+                <div>{weatherData?.main?.temp}</div>
+                <p className="description">
+                    {weatherData && weatherData[0] ? weatherData[0].description : ''}
+                </p>
+            </div>
+
+        }
     </div>
 }
-
-// 5:29:54 is where I got up to in the video 
-// https://www.youtube.com/watch?v=5ZdHfJVAY-s
